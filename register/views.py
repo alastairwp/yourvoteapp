ad3b83252e59da58e42bcfe799ba6a1edf4b5d4f
Alastair Williamson-Pound <alastairwp@gmail.com>
2019-09-10 23:12:03 +0100

    fix register form

--- register/views.py
+++ register/views.py
@@ -3,27 +3,26 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User,auth
 from django.contrib import messages
 
-
 def register(request):
 
     if request.method == 'POST':
-        first_name = request.POST['name']
-        username = request.POST['username']
+        first_name = request.POST['firstname']
+        last_name = request.POST['lastname']
         email = request.POST['email']
         password1 = request.POST['password1']
         password2 = request.POST['password2']
 
         if password1==password2:
-            if User.objects.filter(username=username).exists():
-                messages.info(request,'Username already taken')
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Username already taken')
                 return redirect('/register')
             elif User.objects.filter(email=email).exists():
-                messages.info(request,'Email already taken')
+                messages.info(request, 'Email already taken')
                 return redirect('/register')
             else:
-                user = User.objects.create_user(username=username, email=email, password=password1,first_name=first_name)
+                user = User.objects.create_user(username=email, email=email, password=password1, first_name=first_name, last_name=last_name)
                 user.save()
-                messages.info(request,'User Created')
+                messages.info(request, 'User Created')
                 return redirect('/login')
         else:
             messages.info(request, 'Password Not Matching')
