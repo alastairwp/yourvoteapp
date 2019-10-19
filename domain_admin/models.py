a14c206d3c93fa361ad410d13d1fd0c91540d998
Alastair Williamson-Pound <alastairwp@gmail.com>
2019-10-19 23:55:35 +0100

    user delete cascade usercourse and votes

--- domain_admin/models.py
+++ domain_admin/models.py
@@ -14,8 +14,8 @@ class Course(models.Model):
 
 
 class UserCourse(models.Model):
-    user = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL)
-    course = models.ForeignKey(Course, null=True, blank=True, on_delete=models.SET_NULL)
+    user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, null=True, blank=True, on_delete=models.CASCADE)
     status = models.PositiveIntegerField('Course status', default=0, null=False, blank=False)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
