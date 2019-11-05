98e83b7b99f4774d27a7771846de34eddab160e5
Alastair Williamson-Pound <alastairwp@gmail.com>
2019-11-05 08:25:12 +0000

    set debug False

--- pullingapp/settings.py
+++ pullingapp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('YOURVOTE_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ALLOWED_HOSTS = os.environ.get('YOURVOTE_DJANGO_ALLOWED_HOSTS').split(',')
