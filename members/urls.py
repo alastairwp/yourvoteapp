e3e39250f25d99f91848960ba5f2186d275efe6d
Alastair Williamson-Pound <alastairwp@gmail.com>
2019-11-25 22:07:06 +0000

    reverse is_secure fix

--- members/urls.py
+++ members/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
     re_path(r'^assessment-report/(?P<course_id>\d+)/$', views.assessmentreport, name='course-report'),
     path('password_change/', PasswordChangeView.as_view(template_name='members/password_change_form.html')),
     path('password_change_done/', PasswordChangeDoneView.as_view(template_name='members/password_change_done.html')),
-    path('password_reset/', PasswordResetView.as_view(template_name='members/password_reset_form.html'), {'is_secure': True}),
+    path('password_reset/', PasswordResetView.as_view(template_name='members/password_reset_form.html')),
     path('password_reset/done/', PasswordResetDoneView.as_view(template_name='members/password_reset_done.html'), name="password_reset_done"),
     re_path(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
             PasswordResetConfirmView.as_view(template_name='members/password_reset_confirm.html'), name="password_reset_confirm"),
